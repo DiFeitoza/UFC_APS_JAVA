c8edfbe6a346cfd80e367e7a74a77ee9b5d45a11
difeitoza <diegofeitozabr@gmail.com>
2020-12-07 21:10:03 -0300

    implements GetProductByCode

--- revenda/src/RevendaComArray.java
+++ revenda/src/RevendaComArray.java
@@ -75,29 +75,19 @@ public class RevendaComArray {
         }
     }
 
-
+    public Produto GetProductByCode(int cod){
+        try{
+            for(Produto p : this.produtos){
+                if(p != null){
+                    if(p.getCodigo() == cod){
+                        return p;
+                    }
+                }
+            }
+            throw new Exception("Produto não cadastrado");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
 }
-
-/* 
-• Um método "listaPrecos" que imprime o código, a descrição e o valor de venda de todos os produtos;
-
-
-• Um atributo "produtos" (do tipo array de Produto), privado (utilize a classe Produto criada no exercício anterior);
-• Um atributo privado para servir de índice.
-
-• Um construtor que recebe como argumento a quantidade de produtos e inicializa o atributo "produtos" com um
-array de produtos com o tamanho especificado;
-• Um método "inserirProduto" que recebe como parâmetro um Produto e o insere no array;
-
-• Um método "comprar" que recebe como parâmetro o código de um Produto e quantidade de unidades deste
-produto que foi adquirida pela revenda, percorre o array procurando o Produto e incrementa a quantidade em
-estoque deste produto, caso ele não exista, o método deve imprimir uma mensagem de erro;
-
-• Um método "vender" que recebe como parâmetro o código de um Produto e quantidade de unidades deste
-produto que foi vendida pela revenda, percorre o array procurando o Produto e decrementa a quantidade em
-estoque deste produto, caso ele não exista, o método deve imprimir uma mensagem de erro;
-
-• Um método "consultaPrecoVenda" que recebe como parâmetro o código de um Produto, percorre o array
-procurando o Produto e imprime o valor de venda deste produto, caso ele não exista, o método deve imprimir
-uma mensagem de erro;
-*/
