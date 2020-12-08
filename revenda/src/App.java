56682b584df4297697ca8816c4ef404a42c5e513
difeitoza <diegofeitozabr@gmail.com>
2020-12-07 21:11:48 -0300

    implementados menu inserir, comprar e vender

--- revenda/src/App.java
+++ revenda/src/App.java
@@ -1,8 +1,10 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // String op = "";
+        // Leitor scan = new Leitor();
+        Scanner sc = new Scanner(System.in);
+        String op = "";
 
         Produto p = new Produto(1, "alguma", 1.1, 1.1, 1.1, 2);
         System.out.println(p.getCusto());
@@ -20,37 +22,63 @@ public class App {
         r.listarPrecos();
         System.out.println(p.getQtd_estoque());
 
-        /* while(op != "e"){
+        while(op != "e"){
             System.out.println(
-                "Criar Produto - NP" + "\n" +
-                "Comprar - C" + "\n" +
-                "Vender - V" + "\n" +
-                "Consultar Preço - CP" + "\n" +
-                "Listar Preço - LP" + "\n" +
-                "Consultar Valor de Compra - CVC" + "\n" +
-                "Consultar Custo - CC" + "\n" +
-                "Consultar Margem de Lucro - CML" + "\n" +
-                "Alterar Valor de Compra - AVC" + "\n" +
-                "Alterar Custo - AC" + "\n" +
-                "Alterar Margem de Lucro - AML" + "\n"
+                "----------------------------REVENDACAR------------------------\n" + 
+                "01. Criar Produto\n" +
+                "02. Comprar\n" +
+                "03. Vender\n" +
+                "04. Consultar Preço\n" +
+                "05. Listar Preço\n" +
+                "06. Consultar Valor de Compra\n" +
+                "07. Consultar Custo\n" +
+                "08. Consultar Margem de Lucro\n" +
+                "09. Alterar Valor de Compra\n" +
+                "10. Alterar Custo - AC\n" +
+                "11. Alterar Margem de Lucro\n\n" +
+                "Digite o código à esquerda para acessar a opção correspondente"
             );
-            Scanner sc = new Scanner(System.in);
-            op = sc.next();
-            System.out.println(op);
-        
-            if(op == "NP"){
-                int codigo = Integer.parseInt(sc.next());
-                String descricao = sc.next();
-                double vl_compra = Double.parseDouble(sc.next());
-                double custo = Double.parseDouble(sc.next());
-                double margem_lucro = Double.parseDouble(sc.next());
-                int qtd_estoque = Integer.parseInt(sc.next());
+
+            op = sc.nextLine();
+
+            if(op.contentEquals("01")){
+                System.out.println("Digite um código para o novo produto:");
+                int codigo = Integer.parseInt(sc.nextLine());
+                System.out.println("Digite uma descrição para o novo produto:");
+                String descricao = sc.nextLine();
+                System.out.println("Digite um valor de compra para o novo produto:");
+                double vl_compra = Double.parseDouble(sc.nextLine());
+                System.out.println("Digite um valor de custo para o novo produto:");
+                double custo = Double.parseDouble(sc.nextLine());
+                System.out.println("Digite uma margem de lucro para o novo produto:");
+                double margem_lucro = Double.parseDouble(sc.nextLine());
+                System.out.println("Digite a quantidade em estoque para o novo produto:");
+                int qtd_estoque = Integer.parseInt(sc.nextLine());
 
                 Produto novo = new Produto(codigo, descricao, vl_compra, custo, margem_lucro, qtd_estoque);
                 r.inserirProduto(novo);
-            }
+                System.out.println("\nNovo produto criado!\n" + novo.toString());
+                // #melhoria - inserir um sys.pause aqui!
+            } else if (op.contentEquals("02")){
+                System.out.println("Digite o código do produto:");
+                int cod = Integer.parseInt(sc.nextLine());
+                System.out.println("Digite a quantidade comprada:");
+                int qtd = Integer.parseInt(sc.nextLine());
 
-            sc.close();
-        } */
+                r.comprar(cod, qtd);
+                Produto prod = r.GetProductByCode(cod);
+                System.out.println("\nCompra efetuada!\n" + prod.toString() + "Estoque: " + prod.getQtd_estoque());
+            } else if (op.contentEquals("03")){
+                System.out.println("Digite o código do produto:");
+                int cod = Integer.parseInt(sc.nextLine());
+                System.out.println("Digite a quantidade vendida:");
+                int qtd = Integer.parseInt(sc.nextLine());
+
+                r.vender(cod, qtd);
+                Produto prod = r.GetProductByCode(cod);
+                System.out.println("\nVenda efetuada!\n" + prod.toString() + "Estoque: " + prod.getQtd_estoque());
+            } else  System.out.println("???");
+        }
+        sc.close();
     }
 }
