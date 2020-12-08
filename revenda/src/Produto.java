0b3324020af2f6226db73c3aff1ef7b877ebe327
difeitoza <diegofeitozabr@gmail.com>
2020-12-07 22:50:39 -0300

    ajuste - metodo toString

--- revenda/src/Produto.java
+++ revenda/src/Produto.java
@@ -30,7 +30,7 @@ public class Produto {
 
     @Override
     public String toString(){
-        return "Código: " + this.codigo + "\nDescrição: " + this.descricao + "\nValor: " + this.vl_compra + "\n";
+        return "Código: " + this.codigo + "\nDescrição: " + this.descricao;
     }
 
     public int getCodigo(){
